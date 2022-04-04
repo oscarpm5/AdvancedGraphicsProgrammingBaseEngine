e85ba3b246151d1f6b915d76e47b98f2401b8502
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2022-04-04 19:31:52 +0200

    Assimp model loading included, doesnt work yet!

---
+++ Engine/Code/assimp_model_loading.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "platform.h" //TODO dont include here just declare
+
+struct Model
+{
+	u32 meshIdx;
+	std::vector<u32> materialIdx;
+};
+
+
+
+
+void ProcessAssimpMesh(const aiScene* scene, aiMesh* mesh, Mesh* myMesh, u32 baseMeshMaterialIndex, std::vector<u32>& submeshMaterialIndices);
+
+void ProcessAssimpMaterial(App* app, aiMaterial* material, Material& myMaterial, String directory);
+
+void ProcessAssimpNode(const aiScene* scene, aiNode* node, Mesh* myMesh, u32 baseMeshMaterialIndex, std::vector<u32>& submeshMaterialIndices);
+
+u32 LoadModel(App* app, const char* filename);
+
